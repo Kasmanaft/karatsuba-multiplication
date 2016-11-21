701891471a0c4aa36f7024911b20137b8c89b610
Leonid Sobolievskyi <leonid@sobolievskyi.name>
2016-11-21 12:22:54 +0200

    final changes

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -17,19 +19,43 @@ func init() {
 	flag.Parse()
 }
 
+var Icounter uint64
+
 func main() {
+	Icounter = 0
+	x := "1685287499328328297814655639278583667919355849391453456921116729"
+	y := "7114192848577754587969744626558571536728983167954552999895348492"
+	z, n := Mul(x, y, `12`)
+	fmt.Printf(" %s -> %d\n", z, n)
 }
 
 func Add(x, y string) string {
+	sign := ""
+
+	if strings.HasPrefix(x, `-`) && strings.HasPrefix(y, `-`) {
+		sign = "-"
+		x = strings.TrimLeft(x, "-")
+		y = strings.TrimLeft(y, "-")
+	} else if strings.HasPrefix(x, `-`) {
+		return Sub(y, strings.TrimLeft(x, "-"))
+	} else if strings.HasPrefix(y, `-`) {
+		return Sub(x, strings.TrimLeft(y, "-"))
+	}
+
+	if len(x) == 1 || len(y) == 1 {
+		xn, _ := strconv.Atoi(x)
+		yn, _ := strconv.Atoi(y)
+		return sign + strconv.Itoa(xn+yn)
+	}
+
 	carry := 0
 	result := ""
 	xy := ""
-	n := len(x)
-	xa := []byte(x)
-	ya := []byte(y)
-	for i, _ := range x {
-		xl, _ := strconv.Atoi(string(xa[n-i-1]))
-		yl, _ := strconv.Atoi(string(ya[n-i-1]))
+	n, xs, ys := SameSize(x, y)
+
+	for i := n - 1; i > -1; i-- {
+		xl, _ := strconv.Atoi(string(xs[i]))
+		yl, _ := strconv.Atoi(string(ys[i]))
 		z := strconv.Itoa(xl + yl + carry)
 		if len(z) > 1 {
 			carry = 1
@@ -40,75 +66,118 @@ func Add(x, y string) string {
 		}
 		result = xy + result
 	}
+
 	if carry > 0 {
 		result = "1" + result
 	}
-	return result
+	trimmed := strings.TrimLeft(result, "0")
+	if len(trimmed) == 0 {
+		return `0`
+	}
+	return sign + trimmed
 }
 
 func Sub(x, y string) string {
+	if strings.HasPrefix(x, `-`) && strings.HasPrefix(y, `-`) {
+		x = strings.TrimLeft(x, "-")
+		y = strings.TrimLeft(y, "-")
+		return Sub(y, x)
+	} else if strings.HasPrefix(x, `-`) {
+		return "-" + Add(strings.TrimLeft(x, "-"), y)
+	} else if strings.HasPrefix(y, `-`) {
+		return Add(x, strings.TrimLeft(y, "-"))
+	}
+
+	if len(x) == 1 || len(y) == 1 {
+		xn, _ := strconv.Atoi(x)
+		yn, _ := strconv.Atoi(y)
+		return strconv.Itoa(xn - yn)
+	}
+
 	carry := 0
 	result := ""
-	n := len(x)
-	xa := []byte(x)
-	ya := []byte(y)
+	n, xs, ys := SameSize(x, y)
 	z := ""
-	for i := range x {
-		xl, _ := strconv.Atoi(string(xa[n-i-1]))
-		yl, _ := strconv.Atoi(string(ya[n-i-1]))
+	for i := n - 1; i > -1; i-- {
+		xl, _ := strconv.Atoi(string(xs[i]))
+		yl, _ := strconv.Atoi(string(ys[i]))
 		if yl > xl {
 			z = strconv.Itoa(10 + xl - yl - carry)
 			carry = 1
 		} else {
 			z = strconv.Itoa(xl - yl - carry)
-			carry = 0
+			if z == "-1" {
+				z = "9"
+				carry = 1
+			} else {
+				carry = 0
+			}
 		}
 		result = string(z[0]) + result
 	}
 	if carry > 0 {
-		carry = 0
-		result = ""
-		for i := range x {
-			xl, _ := strconv.Atoi(string(xa[n-i-1]))
-			yl, _ := strconv.Atoi(string(ya[n-i-1]))
-			if xl > yl {
-				z = strconv.Itoa(10 + yl - xl - carry)
-				carry = 1
-			} else {
-				z = strconv.Itoa(yl - xl - carry)
-				carry = 0
-			}
-			result = string(z[0]) + result
-		}
-		result = "-" + result
+		return `-` + Sub(y, x)
 	}
-	return result
+
+	trimmed := strings.TrimLeft(result, "0")
+	if len(trimmed) == 0 {
+		return `0`
+	}
+
+	return trimmed
 }
 
-func Mul(x, y string) string {
-	n := len(x) // Let's assume, both numbers has the same length
-	if n < 2 {
-		xn, _ := strconv.Atoi(x)
-		yn, _ := strconv.Atoi(y)
-		return strconv.Itoa(xn * yn)
+func Mul(x, y, inspected string) (string, uint64) {
+	if strings.HasPrefix(x, `-`) {
+		x = strings.TrimLeft(x, "-")
+	} else if strings.HasPrefix(y, `-`) {
+		y = strings.TrimLeft(y, "-")
 	}
 
-	m := n / 2
-	x1, x2 := SplitString(x)
-	y1, y2 := SplitString(y)
+	if len(x) == 1 && len(y) == 1 {
+		xn, _ := strconv.Atoi(x)
+		yn, _ := strconv.Atoi(y)
+		return strconv.Itoa(xn * yn), 0
+	}
 
-	z2 := Mul(x1, y1)
-	z1 := Mul(Add(x1, x2), Add(y1, y2))
-	z0 := Mul(x2, y2)
+	n, xs, ys := SameSize(x, y)
+	m := int(math.Ceil(float64(n) / 2.0))
 
-	r0 := z2 + strings.Repeat("0", n)
-	r1 := Sub(Sub(z1, z2), z0) + strings.Repeat("0", m)
+	x1, x2 := SplitString(xs)
+	y1, y2 := SplitString(ys)
 
-	return r0 + r1 + z0
+	p1, _ := Mul(x1, y1, inspected)
+	p2, _ := Mul(x2, y2, inspected)
+	p3, _ := Mul(Add(x1, x2), Add(y1, y2), inspected)
+
+	a := p1 + strings.Repeat("0", 2*m)
+	a = Add(a, p2)
+
+	b := Sub(Sub(p3, p1), p2)
+
+	if b == inspected {
+		Icounter = Icounter + 1
+	}
+
+	b = b + strings.Repeat("0", m)
+
+	result := Add(a, b)
+	return result, Icounter
 }
 
 func SplitString(s string) (sl, sr string) {
 	sb := []byte(s)
 	n := len(sb)
 	return string(sb[:n/2]), string(sb[n/2:])
+}
+
+func SameSize(x, y string) (int, string, string) {
+	n := len(x)
+	if n > len(y) {
+		y = strings.Repeat("0", n-len(y)) + y
+	} else if n < len(y) {
+		n = len(y)
+		x = strings.Repeat("0", n-len(x)) + x
+	}
+	return n, x, y
 }
--- main_test.go
+++ main_test.go
@@ -2,67 +2,41 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"io"
 	"os"
 	"testing"
 )
 
 func TestAdd(t *testing.T) {
+	f, err := os.Open("add_cases.csv")
+	if err != nil {
+		t.Error("Can't open test file")
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
 	var v string
-	v = Add("1", "2")
-	if v != "3" {
-		t.Error("Expected 3, got ", v)
-	}
-	v = Add("111111", "666666")
-	if v != "777777" {
-		t.Error("Expected 777777, got ", v)
-	}
-	v = Add("567", "789")
-	if v != "1356" {
-		t.Error("Expected 1356, got ", v)
-	}
-	v = Add("123", "123")
-	if v != "246" {
-		t.Error("Expected 246, got ", v)
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Error("Can't read test file (may be not a csv?)")
+		}
+
+		v = Add(record[0], record[1])
+		if v != record[2] {
+			t.Error("Expected ", record[2], " got ", v)
+		}
+
 	}
 }
 
 func TestSub(t *testing.T) {
-	var v string
-	v = Sub("3", "2")
-	if v != "1" {
-		t.Error("Expected 1, got ", v)
-	}
-	v = Sub("777777", "666666")
-	if v != "111111" {
-		t.Error("Expected 111111, got ", v)
-	}
-	v = Sub("567", "789")
-	if v != "-222" {
-		t.Error("Expected -222, got ", v)
-	}
-	v = Sub("767", "589")
-	if v != "178" {
-		t.Error("Expected 178, got ", v)
-	}
-	v = Sub("876", "642")
-	if v != "234" {
-		t.Error("Expected 234, got ", v)
-	}
-}
-
-func TestSplitString(t *testing.T) {
-	var sl, sr string
-	sl, sr = SplitString("1234")
-	if sl != "12" {
-		t.Error("Expected 12, got ", sl)
-	} else if sr != "34" {
-		t.Error("Expected 34, got ", sr)
-	}
-}
-
-func TestMul(t *testing.T) {
-	f, err := os.Open("test_cases.csv")
+	f, err := os.Open("sub_cases.csv")
 	if err != nil {
 		t.Error("Can't open test file")
 	}
@@ -83,6 +57,93 @@ func TestMul(t *testing.T) {
 		v = Sub(record[0], record[1])
 		if v != record[2] {
 			t.Error("Expected ", record[2], " got ", v)
+		}
+
+	}
+}
+
+func TestSplitString(t *testing.T) {
+	var sl, sr string
+	sl, sr = SplitString("1234")
+	if sl != "12" {
+		t.Error("Expected 12, got ", sl)
+	} else if sr != "34" {
+		t.Error("Expected 34, got ", sr)
+	}
+	sl, sr = SplitString("12345")
+	if sl != "12" {
+		t.Error("Expected 12, got ", sl)
+	} else if sr != "345" {
+		t.Error("Expected 345, got ", sr)
+	}
+}
+
+func TestSameSize(t *testing.T) {
+	var sl, sr string
+	var n int
+	n, sl, sr = SameSize("1234", `12`)
+	if sl != "1234" {
+		t.Error("Expected 1234, got ", sl)
+	} else if sr != "0012" {
+		t.Error("Expected 0012, got ", sr)
+	} else if n != 4 {
+		t.Error("Expected 4, got ", n)
+	}
+
+	n, sl, sr = SameSize(`55`, "123")
+	if sl != "055" {
+		t.Error("Expected 055, got ", sl)
+	} else if sr != "123" {
+		t.Error("Expected 123, got ", sr)
+	} else if n != 3 {
+		t.Error("Expected 3, got ", n)
+	}
+
+	n, sl, sr = SameSize(`55321`, "12345")
+	if sl != "55321" {
+		t.Error("Expected 55321, got ", sl)
+	} else if sr != "12345" {
+		t.Error("Expected 12345, got ", sr)
+	} else if n != 5 {
+		t.Error("Expected 5, got ", n)
+	}
+
+	n, sl, sr = SameSize(`1`, "1234567890")
+	if sl != "0000000001" {
+		t.Error("Expected 0000000001, got ", sl)
+	} else if sr != "1234567890" {
+		t.Error("Expected 1234567890, got ", sr)
+	} else if n != 10 {
+		t.Error("Expected 10, got ", n)
+	}
+}
+
+func TestMul(t *testing.T) {
+	f, err := os.Open("mul_cases.csv")
+	if err != nil {
+		t.Error("Can't open test file")
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	var v string
+	var n uint64
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Error("Can't read test file (may be not a csv?)")
+		}
+
+		Icounter = 0
+		v, n = Mul(record[0], record[1], record[3])
+		if v != record[2] {
+			t.Error("Expected ", record[2], " got ", v)
+		}
+		if record[3] != `0` && record[4] != fmt.Sprintf("%d", n) {
+			t.Error("Expected ", record[3], " encountered ", record[4], "times, got ", n)
 		}
 
 	}
